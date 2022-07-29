0c45d49529cac8d3e405e11dd2a4fe99f769790e
Spydr06 <spydr06@web.de>
2022-07-29 18:16:08 +0200

    Added Ray class

---
+++ include/ray.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vec3.hpp>
+
+namespace raytracing {
+
+class Ray {
+    public:
+        Ray() {}
+        Ray(const Point3 &origin, const Vec3 &direction)
+            : orig(origin), dir(direction)
+        {}
+
+        Point3 origin() const { return orig; }
+        Vec3 direction() const { return dir; }
+
+        Point3 at(double t) const {
+            return orig + t * dir;
+        }
+
+    public:
+        Point3 orig;
+        Vec3 dir;
+};
+
+} // namespace raytracing
---
+++ src/ray.cpp
@@ -0,0 +1,5 @@
+#include <ray.hpp>
+
+namespace raytracing {
+
+}
